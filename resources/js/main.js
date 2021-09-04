b5a56de6f795a91d55e2aae5a166806740f3085b
Jakir Imam <jakir.imam1@gmail.com>
2021-09-04 16:21:37 +0600

    i have made the sticky menu

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,6 +5,19 @@ $(function(){
 });
 
 $(document).ready(function(){
+
+
+  // sticky menu
+
+  $(".js--services-section").waypoint(function(direction){
+    if (direction == "down") {
+      $("nav").addClass("sticky");
+    } else {
+      $("nav").removeClass("sticky");
+    }
+  });
+
+  // mixitup(portfolio)
   var mixer = mixitup('.container');
 
 });
